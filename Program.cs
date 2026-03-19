ebe4e9b907d4955b312f62c82d20fe2255463074
Volodymyr Hodovanets <s29985@pjwstk.edu.pl>
2026-03-19 23:13:38 +0100

    Parse input into integer

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Enter numbers separated by space:");
 string input = Console.ReadLine();
 if (string.IsNullOrEmpty(input))
 {
     Console.WriteLine("Invalid input");
 }
+
+int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
